4745d94045fa562849d1326e4fc4e1c9f9298489
الامبراطور انوس <vhhgvgghhvh1@gmail.com>
2025-08-12 22:36:12 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO-MD;;;2RMh2aoQ#45VsduwmAhC2GKyIvT9JxfjGaKME102TmK3q1EPMDLs", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "967730348571", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
